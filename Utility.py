3a4ab98a517d887c8f55921d98d214836016e9a5
AdrienWitt <adrien@LAPTOP-DBTQGT5K.localdomain>
2023-09-05 16:18:55 +0200

    new version

--- Utility.py
+++ Utility.py
@@ -2,38 +2,74 @@ import os
 import glob
 import numpy as np
 import nibabel as nib
+from nilearn.image import concat_imgs
 
 class NiiProcessor:
-    def __init__(self, root_folder):
+    def __init__(self, root_folder, output_foldername, files_type):
         self.root_folder = root_folder
+        self.output_foldername = output_foldername
+        self.files_type = files_type
 
-    def process_folders(self):
-        participant_folders = glob.glob(os.path.join(self.root_folder, 'p*'))
-        
+    def process_folders(self, files_type):
+       
         for participant_folder in participant_folders:
-            participant_number = participant_folder.split('p')[-1]
-
+            participant = participant_folder[-3:]
+        
             run_folders = glob.glob(os.path.join(participant_folder, 'RUN*'))
-            concatenated_data = []
-
+        
             for run_folder in run_folders:
-                nii_files = glob.glob(os.path.join(run_folder, 'wrMF*.nii'))
-                nii_data_list = [nib.load(nii_file).get_fdata() for nii_file in nii_files]
-                concatenated_data.extend(nii_data_list)
-
-            if concatenated_data:
-                concatenated_data = np.array(concatenated_data)
-                output_filename = f'p{participant_number}_RUN{len(run_folders)}.nii'
-                output_path = os.path.join(self.root_folder, output_filename)
-
-                # Create a new NIfTI image
-                nii_img = nib.Nifti1Image(concatenated_data, affine=None)
-                nib.save(nii_img, output_path)
-
-                print(f"Processed participant {participant_number} and saved {output_filename}")
+                nii_files = glob.glob(os.path.join(run_folder, f'{files_type}*.nii'))
+                concatenated_data = []
+                
+                for file in nii_files:
+                    img = nib.load(file)
+                    concatenated_data.append(img)
+                    
+                concatenated_img = concat_imgs(concatenated_data)
+                run = run_folder[-4:]
+                output_filename = f'{files_type}_{participant}_{run}'
+                output_folder = os.path.join(root_folder, output_foldername)
+                if not os.path.exists(output_folder):
+                    os.mkdir(output_folder)
+                output_path = os.path.join(output_folder, output_filename)
+                nib.save(concatenated_img, output_path)
 
 # Usage
 if __name__ == "__main__":
     root_folder = "/path/to/your/folder"
     nii_processor = NiiProcessor(root_folder)
     nii_processor.process_folders()
+    
+
+root_folder = r'C:\Users\adywi\OneDrive - unige.ch\Documents\Sarcasm_experiment\Adrien\fMRI_study\Preproc_Anlayses\data_done'
+files_type = 'swrMF'
+output_foldername = 'concatenated_images' 
+
+participant_folders = glob.glob(os.path.join(root_folder, 'p*'))
+
+for participant_folder in participant_folders:
+    participant = participant_folder[-3:]
+
+    run_folders = glob.glob(os.path.join(participant_folder, 'RUN*'))
+
+    for run_folder in run_folders:
+        nii_files = glob.glob(os.path.join(run_folder, f'{files_type}*.nii'))
+        concatenated_data = []
+        
+        for file in nii_files:
+            img = nib.load(file)
+            concatenated_data.append(img)
+            
+        concatenated_img = concat_imgs(concatenated_data)
+        run = run_folder[-4:]
+        output_filename = f'{files_type}_{participant}_{run}'
+        output_folder = os.path.join(root_folder, output_foldername)
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        output_path = os.path.join(output_folder, output_filename)
+        nib.save(concatenated_img, output_path)
+
+        
+
+        
+
